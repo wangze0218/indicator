a861475063e855fe27795b5b5546f53c75498bb9
james <james@mail.fameex.info>
2024-06-23 02:12:44 +0800

    ema计算公式修改

--- ema.go
+++ ema.go
@@ -4,6 +4,7 @@ type Ema struct {
 	Weight float64
 	result float64
 	age    uint32
+	prices []float64
 }
 
 func NewEma(weight int32) *Ema {
@@ -11,13 +12,22 @@ func NewEma(weight int32) *Ema {
 }
 
 func (this *Ema) Update(price float64) float64 {
-	if this.age == 0 {
-		this.result = price
-	} else {
-		alpha := 2.0 / (this.Weight + 1.0)
-		this.result = alpha*price + (1-alpha)*this.result
-		//this.result = (2.0 * price + (this.Weight - 1.0) * this.result) / (this.Weight + 1.0)
+	alpha := 2.0 / (this.Weight + 1.0)
+	this.prices = append(this.prices, price)
+	if this.age > uint32(this.Weight) {
+		this.prices = this.prices[1:]
 	}
+	// 计算
+	for i, v := range this.prices {
+		if i == 0 {
+			// 初次更新
+			this.result = v
+		} else {
+			// 后续更新
+			this.result = alpha*v + (1-alpha)*this.result
+		}
+	}
+
 	this.age += 1
 	return this.result
 }
@@ -28,5 +38,4 @@ func (this *Ema) GetPrice() float64 {
 
 func (this *Ema) Clone() *Ema {
 	return &Ema{Weight: this.Weight, result: this.result, age: this.age}
-
 }
