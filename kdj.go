d99e43710451929b594440449102378fe43d6df5
james <james@mail.fameex.info>
2024-07-23 13:41:37 +0400

    sma和kdj处理

--- kdj.go
+++ kdj.go
@@ -1,68 +1,75 @@
 package indicator
 
+import "container/list"
+
 type Kdj struct {
-	n1 int
-	n2 int
-	n3 int
+	n1     int
+	n2     int
+	n3     int
+	kSma   *Sma       // 用于计算 K 值的 SMA
+	dSma   *Sma       // 用于计算 D 值的 SMA
+	dequeH *list.List // 用于存储最近 n1 个 Kline 的 High 值
+	dequeL *list.List // 用于存储最近 n1 个 Kline 的 Low 值
 }
-// NewKdj(9, 3, 3)
+
+// NewKdj 创建一个新的 Kdj 对象
 func NewKdj(n1 int, n2 int, n3 int) *Kdj {
-	return &Kdj{n1:n1, n2:n2, n3:n3}
+	return &Kdj{
+		n1:     n1,
+		n2:     n2,
+		n3:     n3,
+		kSma:   NewSma(),
+		dSma:   NewSma(),
+		dequeH: list.New(),
+		dequeL: list.New(),
+	}
 }
 
-func (this *Kdj)maxHigh(bids []Kline) (h float64) {
-	h = bids[0].High
-	for i := 0; i < len(bids); i++ {
-		if bids[i].High > h {
-			h = bids[i].High
-		}
+// maxHigh 计算最近 n1 个 Kline 的最高值
+func (k *Kdj) maxHigh() float64 {
+	for k.dequeH.Len() > 1 && k.dequeH.Front().Value.(float64) < k.dequeH.Back().Value.(float64) {
+		k.dequeH.Remove(k.dequeH.Front())
 	}
-	return
+	return k.dequeH.Front().Value.(float64)
 }
 
-func (this *Kdj)minLow(bids []Kline) (l float64) {
-	l = bids[0].Low
-	for i := 0; i < len(bids); i++ {
-		if (bids[i].Low < l) {
-			l = bids[i].Low
-		}
+// minLow 计算最近 n1 个 Kline 的最低值
+func (k *Kdj) minLow() float64 {
+	for k.dequeL.Len() > 1 && k.dequeL.Front().Value.(float64) > k.dequeL.Back().Value.(float64) {
+		k.dequeL.Remove(k.dequeL.Front())
 	}
-	return
+	return k.dequeL.Front().Value.(float64)
 }
 
-func (this *Kdj)sma(x []float64, n float64) (r []float64) {
-	r = make([]float64, len(x))
-	for i := 0; i < len(x); i++ {
-		if i == 0 {
-			r[i] = x[i]
-		} else {
-			r[i] = (1.0 * x[i] + (n - 1.0) * r[i - 1]) / n
-		}
+func (k *Kdj) Update(bid Kline) (float64, float64, float64) {
+	// 更新 dequeH 和 dequeL
+	if k.dequeH.Len() >= k.n1 {
+		k.dequeH.Remove(k.dequeH.Front())
 	}
-	return
-}
-
-
-
-func (this *Kdj)Kdj(bids []Kline) (k, d, j []float64) {
-	l := len(bids)
-	rsv := make([]float64, l)
-	j = make([]float64, l)
-	rsv[0] = 50.0
-	for i := 1; i < l; i++ {
-		m := i + 1 - this.n1
-		if m < 0 {
-			m = 0
-		}
-		h := this.maxHigh(bids[m:i + 1])
-		l := this.minLow(bids[m:i + 1])
-		rsv[i] = (bids[i].Close - l) * 100.0 /( h - l)
-		rsv[i] =rsv[i]
+	if k.dequeL.Len() >= k.n1 {
+		k.dequeL.Remove(k.dequeL.Front())
 	}
-	k = this.sma(rsv, float64(this.n2))
-	d = this.sma(k, float64(this.n3))
-	for i := 0; i < l; i++ {
-		j[i] = 3.0 * k[i] - 2.0 * d[i]
+	k.dequeH.PushBack(bid.High)
+	k.dequeL.PushBack(bid.Low)
+
+	// 计算 RSV
+	if k.dequeH.Len() < k.n1 || k.dequeL.Len() < k.n1 {
+		// 如果数据点不足 n1 个，无法计算 KDJ，返回默认值
+		return 0, 0, 0
 	}
-	return
+	h := k.maxHigh()
+	l := k.minLow()
+	var rsv float64
+	if h != l {
+		rsv = (bid.Close - l) * 100.0 / (h - l)
+	} else {
+		rsv = 50.0 // 如果高低相等，RSV 为 50
+	}
+
+	// 更新 K 和 D
+	K := k.kSma.Update(rsv)
+	D := k.dSma.Update(K)
+	J := 3.0*K - 2.0*D
+
+	return K, D, J
 }
--- sma.go
+++ sma.go
@@ -1,48 +1,39 @@
 package indicator
 
 type Sma struct {
-	maxLength int       // 控制切片的最大长度
-	values    []float64 // 存储价格数据的切片
-	sum       float64   // 用于累加计算移动平均值的总和
+	sum   float64 // 用于累加计算移动平均值的总和
+	count int     // 当前数据点的数量
 }
 
-func NewSma(maxLength int) *Sma {
-	return &Sma{
-		maxLength: maxLength,
-		values:    make([]float64, 0, maxLength),
-	}
+// NewSma 创建一个新的 Sma 对象
+func NewSma() *Sma {
+	return &Sma{}
 }
 
-func (this *Sma) Update(price float64) float64 {
-	if len(this.values) >= this.maxLength {
-		// 如果超过最大长度，则移除最旧的数据
-		this.sum -= this.values[0]
-		this.values = this.values[1:]
-	} else {
-		// 如果未达到最大长度，则添加新数据
-		this.values = append(this.values, price)
-	}
-
+// Update 更新 SMA 对象的价格数据并返回当前的移动平均值
+func (s *Sma) Update(price float64) float64 {
 	// 添加新数据并更新总和
-	this.values = append(this.values, price)
-	this.sum += price
+	s.sum += price
+	s.count++
 
 	// 返回移动平均值
-	return this.sum / float64(len(this.values))
+	return s.sum / float64(s.count)
 }
 
-func (this *Sma) GetAverage() float64 {
+// GetAverage 返回当前的移动平均值
+func (s *Sma) GetAverage() float64 {
 	// 计算并返回移动平均值
-	return this.sum / float64(len(this.values))
+	if s.count == 0 {
+		return 0
+	}
+	return s.sum / float64(s.count)
 }
 
-func (this *Sma) Clone() *Sma {
+// Clone 创建并返回当前 SMA 对象的克隆副本
+func (s *Sma) Clone() *Sma {
 	// 创建并返回当前 SMA 对象的克隆副本
-	clone := &Sma{
-		maxLength: this.maxLength,
-		values:    make([]float64, len(this.values)),
-		sum:       this.sum,
+	return &Sma{
+		sum:   s.sum,
+		count: s.count,
 	}
-	copy(clone.values, this.values)
-	return clone
 }
