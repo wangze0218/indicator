c559e4e3794d612a7f5c9476206d487b71f9cb23
james <james@mail.fameex.info>
2024-07-23 15:58:39 +0400

    修改sma

--- sma.go
+++ sma.go
@@ -1,9 +1,11 @@
 package indicator
 
+import "container/list"
+
 // Sma 结构体定义
 type Sma struct {
 	period int
-	count  int
+	queue  *list.List
 	sum    float64
 }
 
@@ -11,26 +13,29 @@ type Sma struct {
 func NewSma(period int) *Sma {
 	return &Sma{
 		period: period,
+		queue:  list.New(),
 	}
 }
 
 // Update 更新 Sma 值
 func (s *Sma) Update(price float64) float64 {
-	if s.count >= s.period {
-		// 先减去最早的值
-		s.sum -= s.sum / float64(s.count)
-	} else {
-		s.count++
+	if s.queue.Len() >= s.period {
+		s.sum -= s.queue.Remove(s.queue.Front()).(float64)
 	}
+	s.queue.PushBack(price)
 	s.sum += price
-	return s.sum / float64(s.count)
+	return s.sum / float64(s.queue.Len())
 }
 
 // Clone 创建并返回当前 Sma 对象的克隆副本
 func (s *Sma) Clone() *Sma {
-	return &Sma{
+	clone := &Sma{
 		period: s.period,
-		count:  s.count,
+		queue:  list.New(),
 		sum:    s.sum,
 	}
+	for e := s.queue.Front(); e != nil; e = e.Next() {
+		clone.queue.PushBack(e.Value)
+	}
+	return clone
 }
